65a4aa08de82d503c7aeba4852f1e24f6f6abf41
Jeff Lai <winfield301@gmail.com>
2016-09-27 17:32:52 +0800

    Add teams update doc

--- lib/teams.rb
+++ lib/teams.rb
@@ -14,15 +14,40 @@ class Teams
       key :description, 'Create a Team'
       key :tags, ['teams']
       parameter do
-        key :name, 'team[name]'
-        key :in, :formData
+        key :name, :data
+        key :in, :body
         key :description, 'Team name'
         key :required, true
+        key :type, :string
+        key :format, :string
       end
     end
   end
 
-  swagger_path '/teams/:id/members' do
+  swagger_path '/teams/{id}' do
+    operation :patch do
+      key :description, 'Update a Team'
+      key :tags, ['teams']
+      parameter do
+        key :name, :id
+        key :in, :path
+        key :description, 'ID of team'
+        key :required, true
+        key :type, :string
+        key :format, :string
+      end
+      parameter do
+        key :name, 'team[name]'
+        key :in, :formData
+        key :description, 'Team name'
+        key :required, true
+        key :type, :string
+        key :format, :string
+      end
+    end
+  end
+
+  swagger_path '/teams/{id}/members' do
     operation :get do
       key :description, 'Returns team by id'
       key :tags, ['teams']
