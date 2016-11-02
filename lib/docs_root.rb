64b737b8b1c16b02bc69a9526fdf4f2ba70bbc0c
Jeff Lai <winfield301@gmail.com>
2016-11-02 11:17:22 +0800

    Add more descriptions to docs

--- lib/docs_root.rb
+++ lib/docs_root.rb
@@ -11,14 +11,22 @@ class DocsRoot
     info do
       key :version, '1.0.0'
       key :title, 'Tower API'
-      key :description, 'Tower API explained'
+      key :description, "
+        文档中的 API 调用地址为测试使用，生产环境 API 地址为 api.tower.im.
+        
+        API 使用 OAuth2 Authorization Code flow 进行认证。 如何进行 OAuth2 认证请参考 
+        [这里](https://github.com/doorkeeper-gem/doorkeeper/wiki/Testing-your-provider-with-OAuth2-gem)
+        以及 [OAuth2 Spec](https://tools.ietf.org/html/rfc6749)
+
+        API 支持 Refresh Token
+        "
     end
 
     security_definition :tower_auth do
       key :type, :oauth2
       key :authorizationUrl, 'https://beta99.tower.im/oauth/authorize'
       key :tokenUrl, 'https://apibeta99.tower.im/oauth/token'
-      key :flow, 'accessCode'
+      key :flow, 'authorization_code'
     end
   end
 end
