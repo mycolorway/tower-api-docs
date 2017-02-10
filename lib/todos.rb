0c6d4e27e1b6f08bf620e1a9f0aa2812cc751eec
Jeff Lai <winfield301@gmail.com>
2017-02-11 00:21:15 +0800

    Add doc for todos comments

--- lib/todos.rb
+++ lib/todos.rb
@@ -154,4 +154,25 @@ class Todos
       end
     end
   end
+
+  swagger_path '/todos/{id}/comments' do
+    operation :post do
+      key :description, 'create comment of todo by id'
+      key :tags, ['todos']
+      parameter do
+        key :name, :id
+        key :in, :path
+        key :description, 'ID of Todo'
+        key :required, true
+        key :type, :string
+        key :format, :string
+      end
+      parameter do
+        key :name, :data
+        key :in, :body
+        key :description, 'Todo comment'
+        key :required, true
+      end
+    end
+  end
 end
