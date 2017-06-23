d79043e43b6f41660577d13c7a8ebd8a90e7952b
袁夏伟 <yxwyoyoyo@gmail.com>
2017-06-23 15:59:15 +0800

    ADD: show team member api (#1)

--- lib/teams.rb
+++ lib/teams.rb
@@ -49,7 +49,25 @@ class Teams
 
   swagger_path '/teams/{id}/members' do
     operation :get do
-      key :description, 'Returns team by id'
+      key :description, 'Returns team members by id'
+      key :tags, ['teams']
+      parameter do
+        key :name, :id
+        key :in, :path
+        key :description, 'ID of team'
+        key :required, true
+        key :type, :string
+        key :format, :string
+      end
+      response 200 do
+        key :description, 'teams response'
+      end
+    end
+  end
+
+  swagger_path '/teams/{id}/member' do
+    operation :get do
+      key :description, 'Return member by id'
       key :tags, ['teams']
       parameter do
         key :name, :id
